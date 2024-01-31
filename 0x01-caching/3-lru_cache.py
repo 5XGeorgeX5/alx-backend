ec9229ed83aab045676bf42245339ddf7b35373a
George Raafat <georgexraafat@gmail.com>
2024-02-01 01:38:07 +0200

    task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" LRU caching module
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class """
+
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ puts an item in cache_data """
+        if key is None or item is None:
+            return
+        if (len(self.cache_data) == self.MAX_ITEMS):
+            removed = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {removed[0]}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ gets an item from cache_data """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
